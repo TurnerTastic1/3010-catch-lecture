434093b2de0db7be435d1d3140558fb63e18e1fb
Turner Naef <Turner.Naef@gmail.com>
2025-01-28 14:56:08 -0700

    finish functions and tests

--- functions_to_implement.cpp
+++ functions_to_implement.cpp
@@ -3,52 +3,191 @@
 /* String functions section */
 
 // Splits a single string on separator into a vector of strings
-std::vector<std::string> Split(std::string whole, std::string separator);
+std::vector<std::string> Split(std::string whole, std::string separator)
+{
+  std::vector<std::string> result;
+  size_t pos = 0;
+  while ((pos = whole.find(separator)) != std::string::npos)
+  {
+    result.push_back(whole.substr(0, pos));
+    whole.erase(0, pos + separator.length());
+  }
+  result.push_back(whole);
+  return result;
+}
 
 // takes two strings and returns a new string that is the result of removing all occurrences of s2 from s1.
-std::string RemoveAllSubstrings(std::string s1, std::string s2);
+std::string RemoveAllSubstrings(std::string s1, std::string s2)
+{
+  size_t pos = 0;
+  while ((pos = s1.find(s2)) != std::string::npos)
+  {
+    s1.erase(pos, s2.length());
+  }
+  return s1;
+}
 
 // takes two strings and returns a new string that is the result of removing the first occurrence of s2 from s1.
-std::string RemoveFirstSubstring(std::string s1, std::string s2);
+std::string RemoveFirstSubstring(std::string s1, std::string s2)
+{
+  size_t pos = s1.find(s2);
+  if (pos != std::string::npos)
+  {
+    s1.erase(pos, s2.length());
+  }
+  return s1;
+}
 
 // Joins all strings in a vector together, using the glue string in between them
-std::string Join(std::vector<std::string> pieces, std::string glue);
+std::string Join(std::vector<std::string> pieces, std::string glue)
+{
+  std::string result = "";
+  for (int i = 0; i < (int)pieces.size(); i++)
+  {
+    result += pieces[i];
+    if (i != (int)pieces.size() - 1)
+    {
+      result += glue;
+    }
+  }
+  return result;
+}
 
 // takes two vectors of integers, a and b. The function then removes elements from a if they are also in b.
 // If the integer is in b, but not in a, nothing happens.
-std::vector<int> MatchVectors(std::vector<int> a, std::vector<int> b);
+std::vector<int> MatchVectors(std::vector<int> a, std::vector<int> b)
+{
+  std::vector<int> result;
+  for (int i = 0; i < (int)a.size(); i++)
+  {
+    bool found = false;
+    for (int j = 0; j < (int)b.size(); j++)
+    {
+      if (a[i] == b[j])
+      {
+        found = true;
+        break;
+      }
+    }
+    if (!found)
+    {
+      result.push_back(a[i]);
+    }
+  }
+  return result;
+}
 
 // divides an input integer by 2 until it is impossible to do so, then returns the final number.
 // (16 = 2 * 2 * 2 * 2 * 1 -> 1, 7 -> 7, 26 = 2 * 13 -> 13, 52 = 2 * 2 * 13 -> 13)
-int RemoveTwos(int original);
+int RemoveTwos(int original)
+{
+  while (original % 2 == 0)
+  {
+    original /= 2;
+  }
+  return original;
+}
 
 // takes a vector of integers and removes all elements evenly divisible by the passed in int
-std::vector<int> MultiplesFilter(std::vector<int>, int divides_by);
+std::vector<int> MultiplesFilter(std::vector<int> v, int divides_by)
+{
+  std::vector<int> result;
+  for (int i = 0; i < (int)v.size(); i++)
+  {
+    if (v[i] % divides_by != 0)
+    {
+      result.push_back(v[i]);
+    }
+  }
+  return result;
+}
 
 // returns a vector with true for even numbers and false for odd numbers
-std::vector<bool> EvenMask(std::vector<int>);
+std::vector<bool> EvenMask(std::vector<int> nums)
+{
+  std::vector<bool> result;
+  for (int i = 0; i < (int)nums.size(); i++)
+  {
+    result.push_back(nums[i] % 2 == 0);
+  }
+  return result;
+}
 
 // returns a vector with true for odd numbers and false for even numbers
-std::vector<bool> OddMask(std::vector<int>);
+std::vector<bool> OddMask(std::vector<int> nums)
+{
+  std::vector<bool> result;
+  for (int i = 0; i < (int)nums.size(); i++)
+  {
+    result.push_back(nums[i] % 2 != 0);
+  }
+  return result;
+}
 
 // Sums all numbers in a vector and returns the resulting value
-int Sum(std::vector<int> nums);
+int Sum(std::vector<int> nums)
+{
+  int sum = 0;
+  for (int i = 0; i < (int)nums.size(); i++)
+  {
+    sum += nums[i];
+  }
+  return sum;
+}
 
 // Multiplies all numbers in a vector together and returns the resulting value
-int Product(std::vector<int> nums);
+int Product(std::vector<int> nums)
+{
+  int product = 1;
+  for (int i = 0; i < (int)nums.size(); i++)
+  {
+    product *= nums[i];
+  }
+  return product;
+}
 
 // Adds an integer n to each element of a given vector
-std::vector<int> VectorPlusN(std::vector<int> v, int n);
+std::vector<int> VectorPlusN(std::vector<int> v, int n)
+{
+  for (int i = 0; i < (int)v.size(); i++)
+  {
+    v[i] += n;
+  }
+  return v;
+}
 
 // Multiples an integer n with each element of a given vector
-std::vector<int> VectorTimesN(std::vector<int> v, int n);
+std::vector<int> VectorTimesN(std::vector<int> v, int n)
+{
+  for (int i = 0; i < (int)v.size(); i++)
+  {
+    v[i] *= n;
+  }
+  return v;
+}
 
 // takes in two integers and returns a vector of size n with
 // values n*1, n*2, n*3... up to n*m
-std::vector<int> Multiples(int n, int m);
+std::vector<int> Multiples(int n, int m)
+{
+  std::vector<int> result;
+  for (int i = 1; i <= m; i++)
+  {
+    result.push_back(n * i);
+  }
+  return result;
+}
 
 // takes an integer n that is >= 0 and returns a vector of all squares up to n^n (1^1, 2^2, 3^3, .... n^n)
-std::vector<int> SquaresUntil(int n);
+std::vector<int> SquaresUntil(int n)
+{
+  std::vector<int> result;
+  for (int i = 1; i <= n; i++)
+  {
+    result.push_back(i * i);
+  }
+  return result;
+}
 
 // takes an int, n, and returns the nth value of the fibonacci sequence (1, 1, 2, 3, 5, 8, 13, ...)
 int NthFibonacci(int n)
@@ -68,23 +207,59 @@ int Factorial(int n)
 }
 
 // returns -1 if the number is negative and 1 if positive
-int Sign(int num) { return num < 0 ? -1 : 1; }
+int Sign(int num)
+{
+  return num < 0 ? -1 : 1;
+}
 
 // takes two vectors of doubles, a and b. The function then removes elements from a if they are also in b.
 // If the double is in b, but not in a, nothing happens.
-std::vector<double> MatchVectors(std::vector<double> a, std::vector<double> b);
+std::vector<double> MatchVectors(std::vector<double> a, std::vector<double> b)
+{
+  std::vector<double> result;
+  for (int i = 0; i < (int)a.size(); i++)
+  {
+    bool found = false;
+    for (int j = 0; j < (int)b.size(); j++)
+    {
+      if (a[i] == b[j])
+      {
+        found = true;
+        break;
+      }
+    }
+    if (!found)
+    {
+      result.push_back(a[i]);
+    }
+  }
+  return result;
+}
 
 // takes a vector of doubles and removes all elements evenly divisible by the passed in double
-std::vector<double> MultiplesFilter(std::vector<double>, double divides_by);
+std::vector<double> MultiplesFilter(std::vector<double> v, double divides_by)
+{
+  std::vector<double> result;
+  for (int i = 0; i < (int)v.size(); i++)
+  {
+    if (fmod(v[i], divides_by) != 0)
+    {
+      result.push_back(v[i]);
+    }
+  }
+  return result;
+}
 
 // returns a vector with true for numbers greater than the second parameters and false for those less than or equal to
-std::vector<bool> GreaterMask(std::vector<double> nums, double greater_than);
-
-// returns a vector with true for numbers less than the second parameters and false for those greater than or equal to
-std::vector<bool> LessMask(std::vector<double> nums, double less_than);
-
-// returns a vector with true for numbers greater than the second parameters and false for those less than or equal to
-std::vector<bool> GreaterMask(std::vector<int> nums, int greater_than);
+std::vector<bool> GreaterMask(std::vector<double> nums, double greater_than)
+{
+  std::vector<bool> result;
+  for (int i = 0; i < (int)nums.size(); i++)
+  {
+    result.push_back(nums[i] > greater_than);
+  }
+  return result;
+}
 
 // returns a vector with true for numbers less than the second parameters and false for those greater than or equal to
 std::vector<bool> LessMask(std::vector<int> nums, int less_than)
--- test.cpp
+++ test.cpp
@@ -2,6 +2,129 @@
 #include "catch.hpp"
 #include "functions_to_implement.hpp"
 
+TEST_CASE("Test Split function")
+{
+  std::vector<std::string> split = Split("Hello,World", ",");
+  REQUIRE(split[0] == "Hello");
+  REQUIRE(split[1] == "World");
+}
+
+TEST_CASE("Test RemoveAllSubstrings function")
+{
+  REQUIRE(RemoveAllSubstrings("Hello,World", ",") == "HelloWorld");
+}
+
+TEST_CASE("Test RemoveFirstSubstring function")
+{
+  REQUIRE(RemoveFirstSubstring("Hello,World", ",") == "HelloWorld");
+}
+
+TEST_CASE("Test Join function")
+{
+  std::vector<std::string> pieces = {"Hello", "World"};
+  REQUIRE(Join(pieces, ",") == "Hello,World");
+}
+
+TEST_CASE("Test MatchVectors function")
+{
+  std::vector<int> a = {1, 2, 3, 4, 5};
+  std::vector<int> b = {2, 4};
+  std::vector<int> matched = MatchVectors(a, b);
+  REQUIRE(matched[0] == 1);
+  REQUIRE(matched[1] == 3);
+  REQUIRE(matched[2] == 5);
+}
+
+TEST_CASE("Test RemoveTwos function")
+{
+  REQUIRE(RemoveTwos(4) == 1);
+  REQUIRE(RemoveTwos(6) == 3);
+  REQUIRE(RemoveTwos(8) == 1);
+}
+
+TEST_CASE("Test MultiplesFilter function")
+{
+  std::vector<int> nums = {1, 2, 3, 4, 5};
+  std::vector<int> filtered = MultiplesFilter(nums, 2);
+  REQUIRE(filtered[0] == 1);
+  REQUIRE(filtered[1] == 3);
+  REQUIRE(filtered[2] == 5);
+}
+
+TEST_CASE("Test EvenMask function")
+{
+  std::vector<int> nums = {1, 2, 3, 4, 5};
+  std::vector<bool> even = EvenMask(nums);
+  REQUIRE(even[0] == false);
+  REQUIRE(even[1] == true);
+  REQUIRE(even[2] == false);
+  REQUIRE(even[3] == true);
+  REQUIRE(even[4] == false);
+}
+
+TEST_CASE("Test OddMask function")
+{
+  std::vector<int> nums = {1, 2, 3, 4, 5};
+  std::vector<bool> odd = OddMask(nums);
+  REQUIRE(odd[0] == true);
+  REQUIRE(odd[1] == false);
+  REQUIRE(odd[2] == true);
+  REQUIRE(odd[3] == false);
+  REQUIRE(odd[4] == true);
+}
+
+TEST_CASE("Test Sum (int) function")
+{
+  std::vector<int> nums = {1, 2, 3, 4, 5};
+  REQUIRE(Sum(nums) == 15);
+}
+
+TEST_CASE("Test Product (int) function")
+{
+  std::vector<int> nums = {1, 2, 3, 4, 5};
+  REQUIRE(Product(nums) == 120);
+}
+
+TEST_CASE("Test VectorPlusN (int) function")
+{
+  std::vector<int> v = {10, 20, 30, 40, 50};
+  std::vector<int> added = VectorPlusN(v, 5);
+  REQUIRE(added[0] == 15);
+  REQUIRE(added[1] == 25);
+  REQUIRE(added[2] == 35);
+  REQUIRE(added[3] == 45);
+  REQUIRE(added[4] == 55);
+}
+
+TEST_CASE("Test VectorTimesN (int) function")
+{
+  std::vector<int> v = {10, 20, 30, 40, 50};
+  std::vector<int> multiplied = VectorTimesN(v, 5);
+  REQUIRE(multiplied[0] == 50);
+  REQUIRE(multiplied[1] == 100);
+  REQUIRE(multiplied[2] == 150);
+  REQUIRE(multiplied[3] == 200);
+  REQUIRE(multiplied[4] == 250);
+}
+
+TEST_CASE("Test Multiples (int) function")
+{
+  std::vector<int> multiples = Multiples(5, 5);
+  REQUIRE(multiples[0] == 5);
+  REQUIRE(multiples[1] == 10);
+  REQUIRE(multiples[2] == 15);
+  REQUIRE(multiples[3] == 20);
+  REQUIRE(multiples[4] == 25);
+}
+
+TEST_CASE("Test SquaresUntil function")
+{
+  std::vector<int> squares = SquaresUntil(10);
+  REQUIRE(squares[0] == 1);
+  REQUIRE(squares[1] == 4);
+  REQUIRE(squares[2] == 9);
+}
+
 TEST_CASE("Test NthFibonacci function")
 {
   REQUIRE(NthFibonacci(0) == 0);
@@ -24,6 +147,36 @@ TEST_CASE("Test Sign (int) function")
 {
   REQUIRE(Sign(-1) == -1);
   REQUIRE(Sign(1) == 1);
+}
+
+TEST_CASE("Test MatchVectors (double) function")
+{
+  std::vector<double> a = {1.0, 2.0, 3.0, 4.0, 5.0};
+  std::vector<double> b = {2.0, 4.0};
+  std::vector<double> matched = MatchVectors(a, b);
+  REQUIRE(matched[0] == 1.0);
+  REQUIRE(matched[1] == 3.0);
+  REQUIRE(matched[2] == 5.0);
+}
+
+TEST_CASE("Test MultiplesFilter (double) function")
+{
+  std::vector<double> nums = {1.0, 2.0, 3.0, 4.0, 5.0};
+  std::vector<double> filtered = MultiplesFilter(nums, 2.0);
+  REQUIRE(filtered[0] == 1.0);
+  REQUIRE(filtered[1] == 3.0);
+  REQUIRE(filtered[2] == 5.0);
+}
+
+TEST_CASE("Test GreaterMask (double) function")
+{
+  std::vector<double> nums = {10.0, 20.0, 30.0, 40.0, 50.0};
+  std::vector<bool> greater = GreaterMask(nums, 30.0);
+  REQUIRE(greater[0] == false);
+  REQUIRE(greater[1] == false);
+  REQUIRE(greater[2] == false);
+  REQUIRE(greater[3] == true);
+  REQUIRE(greater[4] == true);
 }
 
 TEST_CASE("Test LessMask (int) function")
